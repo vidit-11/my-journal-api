c031649402abdea231073902fda118f345b4bd32
Vidit Singh Chauhan <131014978+vidit-11@users.noreply.github.com>
2026-01-01 18:31:57 +0530

    first commit

---
+++ journal/src/main/java/com/ejournal/journal/controller/HealthCheck.java
@@ -0,0 +1,12 @@
+package com.ejournal.journal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "ok";
+    }
+}
