33f531cd18896aeb7c8229ac5fa7609bce38ffa8
Vidit Singh Chauhan <131014978+vidit-11@users.noreply.github.com>
2026-01-02 20:02:00 +0530

    Refactor JournalEntry class to use Lombok annotations

--- journal/src/main/java/com/ejournal/journal/entity/JournalEntry.java
+++ journal/src/main/java/com/ejournal/journal/entity/JournalEntry.java
@@ -1,47 +1,20 @@
 package com.ejournal.journal.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection = "journal_entries" )
+@Data
+
 public class JournalEntry {
     @Id
     private ObjectId id; //primary key
     private String title;
     private String content;
     private LocalDateTime date;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 }
