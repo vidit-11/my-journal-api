9477dff4b69b19b67b30d9149d5ef28fdbdc17fc
Vidit Singh Chauhan <131014978+vidit-11@users.noreply.github.com>
2026-01-03 12:41:32 +0530

    Create UserRepo interface for MongoDB repository

---
+++ journal/src/main/java/com/ejournal/journal/repository/UserRepo.java
@@ -0,0 +1,8 @@
+package com.ejournal.journal.repository;
+
+import com.ejournal.journal.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepo extends MongoRepository<User, ObjectId> {
+}
