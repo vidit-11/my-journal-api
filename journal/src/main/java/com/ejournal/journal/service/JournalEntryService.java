ab273f64f3973b56e293f40232f13a3412971004
Vidit Singh Chauhan <131014978+vidit-11@users.noreply.github.com>
2026-01-03 22:59:07 +0530

    Enhance JournalEntryService with user handling

--- journal/src/main/java/com/ejournal/journal/service/JournalEntryService.java
+++ journal/src/main/java/com/ejournal/journal/service/JournalEntryService.java
@@ -1,22 +1,45 @@
 package com.ejournal.journal.service;
 
 import com.ejournal.journal.entity.JournalEntry;
+import com.ejournal.journal.entity.User;
 import com.ejournal.journal.repository.JournalEntryRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class JournalEntryService {
 
     @Autowired
     private JournalEntryRepo journalEntryRepo;
+    @Autowired
+    private UserService userService;
+
+    public void saveEntry(JournalEntry journalEntry, String userName){
+        try {
+
+            User user = userService.findByUsername(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepo.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            userService.saveEntry(user);
+        } catch (Exception e) {
+            log.error( "Exception",e);
+        }
+    }
 
     public void saveEntry(JournalEntry journalEntry){
-        journalEntryRepo.save(journalEntry);
+        try {
+            journalEntry.setDate(LocalDateTime.now());
+            journalEntryRepo.save(journalEntry);
+        } catch (Exception e) {
+            log.error( "Exception",e);
+        }
     }
 
     public List<JournalEntry> getAllEntries(){
@@ -27,8 +50,11 @@ public class JournalEntryService {
         return journalEntryRepo.findById(id);
     }
 
-    public void deleteById(ObjectId id){
+    public void deleteById(ObjectId id, String userName){
         journalEntryRepo.deleteById(id);
+        User user= userService.findByUsername(userName);
+        user.getJournalEntries().removeIf(x ->x.getId().equals(id));
+        userService.saveEntry(user);
     }
 
 }
