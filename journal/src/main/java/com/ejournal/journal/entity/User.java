29bfb132940efc14501a75bc1a65b55044ef13a7
Vidit Singh Chauhan <131014978+vidit-11@users.noreply.github.com>
2026-01-03 12:42:33 +0530

    Create User entity for MongoDB with Lombok support

---
+++ journal/src/main/java/com/ejournal/journal/entity/User.java
@@ -0,0 +1,25 @@
+package com.ejournal.journal.entity;
+
+import lombok.Data;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "users" )
+@Data
+public class User {
+    @Id
+    private ObjectId id; //primary key
+    @Indexed(unique = true)
+    @NonNull
+    private String username; //unique username
+    @NonNull
+    private String password;
+    @DBRef
+    private List<JournalEntry> journalEntries= new ArrayList<>();
+}
